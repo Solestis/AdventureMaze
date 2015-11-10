91b974055c45cdfd63942f56deae301c81dfb563
Mark <mark.happel01@gmail.com>
2015-11-10 12:12:34 +0100

    Added a maze generator class, which generates an int[][]. Then this is converted to Tile[][] in the Maze class.

--- src/adventuremaze/Main.java
+++ src/adventuremaze/Main.java
@@ -3,7 +3,9 @@ package adventuremaze;
 public class Main {
 
 	public Main() {
-		System.out.println("Test!");
+		Maze maze = new Maze(10, 10);
+		maze.display();
+		maze.printMazeInts();
 	}
 
 	public static void main(String[] args) {
--- src/adventuremaze/Tile.java
+++ src/adventuremaze/Tile.java
@@ -2,8 +2,31 @@ package adventuremaze;
 
 public class Tile {
 
+	private int identifier;
+	
 	public Tile() {
-		// TODO Auto-generated constructor stub
+		this(0);
+	}
+	
+	public Tile(int _identifier) {
+		identifier = _identifier;
+	}
+	
+	@Override
+	public String toString() {
+		return Integer.toString(identifier);
+	}
+
+	public int getIdentifier() {
+		return identifier;
+	}
+	
+	public void printNorth() {
+		System.out.print((identifier & 1) == 0 ? "+---" : "+   ");
+	}
+	
+	public void printWest() {
+		System.out.print((identifier & 8) == 0 ? "|   " : "    ");
 	}
 
 }
